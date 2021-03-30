822a6e1feffc6abae3bf0afba5ebe6dbfbdf3646
ChengHan <tankhc1@163.com>
2021-03-29 21:17:52 -0400

    first commit

---
+++ WGAN_Improve_training/train.py
@@ -0,0 +1,112 @@
+'''Training for DCGAN on MNIST dataset with Discriminator and Generator imported from model.py'''
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model import Critic, Generator, initialize_weights
+from utils import gradient_penalty
+
+
+# Hyper-para
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+LEARNING_RATE = 1e-4
+BATCH_SIZE = 128
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+Z_DIM = 100 # Noise_dim
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+
+
+# NEW ADDED
+CRITIC_ITERATIONS = 5
+LAMBDA_GP = 10
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
+        ),
+    ]
+)
+
+dataset = datasets.MNIST(root=r'C:\Users\hanch\PycharmProjects\GAN_origin\dataset', train=True, transform=transforms, download=True)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+critic = Critic(CHANNELS_IMG, FEATURES_DISC).to(device)
+initialize_weights(gen)
+initialize_weights(critic)
+
+# change into RMSprop instead of Adam
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0,0.9))
+opt_critic = optim.Adam(critic.parameters(), lr=LEARNING_RATE, betas=(0.0,0.9))
+
+fixed_noise = torch.randn((32, Z_DIM, 1, 1)).to(device)
+writer_real = SummaryWriter(f'logs/real')
+writer_fake = SummaryWriter(f'logs/fake')
+step = 0
+
+gen.train()
+critic.train()
+
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (real, _) in enumerate(dataloader):
+        real = real.to(device)
+        noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+        fake = gen(noise)
+
+        for _ in range(CRITIC_ITERATIONS):
+            noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+            fake = gen(noise)
+            critic_real = critic(real).reshape(-1)
+            critic_fake = critic(fake).reshape(-1)
+            # New added
+            gp = gradient_penalty(critic, real, fake, device=device)
+
+            # original loss + lambda * gp
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
+            )
+            critic.zero_grad()
+            loss_critic.backward(retain_graph=True)
+            opt_critic.step()
+
+        # We are not using clip right now, we use gradient penalty instead.
+
+        #Train Generator: min E[critic(gen_fake)]
+        output = critic(fake).reshape(-1)
+        loss_gen = -torch.mean(output)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+    # Print losses occasionally and print to tensorboard
+        if batch_idx % 100 == 0:
+            print(
+                f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(dataloader)} \
+                  Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
+            )
+
+            with torch.no_grad():
+                fake = gen(fixed_noise)
+                # take out (up to) 32 examples
+                img_grid_real = torchvision.utils.make_grid(
+                    real[:32], normalize=True
+                )
+                img_grid_fake = torchvision.utils.make_grid(
+                    fake[:32], normalize=True
+                )
+
+                writer_real.add_image("Real", img_grid_real, global_step=step)
+                writer_fake.add_image("Fake", img_grid_fake, global_step=step)
+
+            step += 1
+
